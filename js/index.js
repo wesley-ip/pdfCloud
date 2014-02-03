23b85388c3d5118abf34bc82d9fbc5c8595fd629
Wesley Henrique Francisco <wesley@ipestudio.com.br>
2014-02-03 08:47:25 -0200

    updt get.picture

--- js/index.js
+++ js/index.js
@@ -10,38 +10,19 @@ var app = {
 
 	onDeviceReady: function() {
 
-		var fileTransfer = new FileTransfer();
-		var uri = encodeURI("http://192.168.10.37/macmini/steve-jobs.pdf");
-		var filePath = uri.substring(uri.lastIndexOf('/')+1);
+		var options;
 
-		// fileTransfer.onprogress = function(progressEvent) {
-		// 	if (progressEvent.lengthComputable) {
-		// 		loadingStatus.setPercentage(progressEvent.loaded / progressEvent.total);
-		// 	} else {
-		// 		loadingStatus.increment();
-		// 	}
-		// };
+		Camera.getPicture(
 
-		fileTransfer.download (
-			uri, // source
-			filePath, //target
-			function(entry) { // successCallback
-				alert("download complete: " + entry.fullPath);
+			function() {
+				alert("sucesso");
 			},
 
-			function(error) { // errorCallback
-				alert("download error source " + error.source);
-				alert("download error target " + error.target);
-				alert("upload error code" + error.code);
+			function() {
+				alert("falhou")
 			},
 
-			false, // trustAllHosts
-
-			{
-				headers: { // options
-					"Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
-				}
-			}
+			options
 		);
 
 		// var ref = window.open('http://www.google.com.br', '_blank', 'location=yes');
