49f851e2d90fda4a2ef79daaa669a83ef489d4fb
Wesley Henrique Francisco <wesley@ipestudio.com.br>
2014-02-03 10:08:22 -0200

    updt - dialogs - camera

--- js/index.js
+++ js/index.js
@@ -9,27 +9,40 @@ var app = {
 	},
 
 	onDeviceReady: function() {
-
-		var options;
-
-		Camera.getPicture(
-
-			function() {
-				alert("sucesso");
-			},
-
-			function() {
-				alert("falhou")
-			},
-
-			options
-		);
-
 		// var ref = window.open('http://www.google.com.br', '_blank', 'location=yes');
 		// ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
 		// ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
 		// ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
 		// ref.addEventListener('exit', function(event) { alert(event.type); });
+	},
+
+	capturePhoto: function(){
+		navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, {sourceType:1,quality:60});
+	}
+
+	onPhotoDataSuccess: function(imageData) {
+		var image = document.getElementById('photoImage'); // pegando elemento img
+		image.style.display = 'block'; // aplica block ao elemento image
+		image.src = "data:image/jpeg;base64," + imageData; // jogando a imagem no elemento
+
+		navigator.notification.alert(
+			'Sua foto foi tirada com sucesso', // mensagem
+			null, // callback
+			'Foto', // titulo
+			'OK' // nome do botao
+		);
+
+	},
+
+	onFail: function(message) {
+		alert('Erro: '+message);
+
+		navigator.notification.alert(
+			'Ocorreu um erro no processo, tente novamente.', // mensagem
+			null, // callback
+			'Foto', // titulo
+			'OK' // nome do botao
+		);
 	}
 
 };
